fd920ecb29a55ce6dcbec42216659b8105123c2b
Ishaan Singh <ishaans.chawla@gmail.com>
2017-08-10 11:25:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML = "New value";
 
 //Move the image
 
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function(){
     img.style.maginLeft='100px';
 }
