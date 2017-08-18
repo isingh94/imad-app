fe7a0e41d35ee394b045dfa2938f81e8f6d9bfec
Ishaan Singh <ishaans.chawla@gmail.com>
2017-08-18 16:59:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ button.onlick = function () {
     //Capture the response and store it in a variable
     
     //Render the variable in the correct span
-    counter=counter+1;
+    counter = counter+1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
     
